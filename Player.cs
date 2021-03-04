003e4ee6ef275a653b0ff1b71a2875da77eac6af
nelml <konpaku.youmi@gmail.com>
2021-03-04 16:57:05 +0900

    4-2 new class file

---
+++ Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cch
+{
+    class Player
+    {
+        public string name;
+        public int hp;
+
+        public void Attack()
+        {
+            Console.WriteLine(this.name + "は攻撃した");
+        }
+
+        public void Defense()
+        {
+            Console.WriteLine(this.name + "は防御した");
+
+        }
+    }
+}
