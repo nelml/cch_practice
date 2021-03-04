f5a6d2f234a92f412d00379b7d5d42a5bf0c4654
nelml <konpaku.youmi@gmail.com>
2021-03-04 16:32:03 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace cch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int money;
+            string name;
+
+            money = 5000;
+            name = "きたむら";
+
+            Console.WriteLine(money);
+            Console.WriteLine(name);
+
+        }
+    }
+}
